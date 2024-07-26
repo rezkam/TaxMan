ae8386b0bad242dc2211940d748b8315e09be3db
Rez <hi@rez.nu>
2024-07-26 03:41:12 +0200

    update model to add PeriodType

--- model/model.go
+++ model/model.go
@@ -1,6 +1,26 @@
 package model
 
-import "time"
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrNotFound = errors.New("not found")
+)
+
+// PeriodType defines the type of period for a tax record
+type PeriodType string
+
+const (
+	Yearly  PeriodType = "yearly"
+	Monthly PeriodType = "monthly"
+	Weekly  PeriodType = "weekly"
+	Daily   PeriodType = "daily"
+)
+
+// ValidPeriodTypes contains all valid period types
+var ValidPeriodTypes = []PeriodType{Yearly, Monthly, Weekly, Daily}
 
 // TaxRecord represents a tax record with appropriate types.
 type TaxRecord struct {
@@ -8,6 +28,7 @@ type TaxRecord struct {
 	TaxRate      float64
 	StartDate    time.Time
 	EndDate      time.Time
+	PeriodType   PeriodType
 }
 
 // TaxQuery represents a query for a tax rate.
